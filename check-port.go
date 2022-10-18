8f40a3f784b39151015666cae36c3e17a852d3f0
Mikhail Yudin <fagci.nsk@gmail.com>
2022-10-19 04:17:10 +0700

    fix: output count not equals parameter

--- check-port.go
+++ check-port.go
@@ -13,16 +13,15 @@ import (
 var (
 	port       string
 	workers    int
-	count      int64
+	count      uint64
 	timeout    time.Duration
-	stdinMutex sync.Mutex
-	countMutex sync.Mutex
+	printMutex sync.Mutex
 )
 
 func init() {
 	flag.StringVar(&port, "p", "80", "port")
 	flag.IntVar(&workers, "w", 256, "workers count")
-	flag.Int64Var(&count, "c", 0, "count of IPs to gather (0 = infinite)")
+	flag.Uint64Var(&count, "c", 0, "count of IPs to gather (0 = infinite)")
 	flag.DurationVar(&timeout, "t", 750*time.Millisecond, "connection timeout")
 }
 
@@ -35,45 +34,62 @@ func CheckPort(host string, port string) bool {
 	return false
 }
 
-func Work(scanner *bufio.Scanner, limited bool, wg *sync.WaitGroup) {
+func Work(ch chan string, limited bool, wg *sync.WaitGroup) {
 	defer wg.Done()
+
 	for {
-		stdinMutex.Lock()
-		if !scanner.Scan() {
+		ip, ok := <-ch
+		if !ok {
 			break
 		}
-		ip := scanner.Text()
-		stdinMutex.Unlock()
+
 		if !CheckPort(ip, port) {
 			continue
 		}
+
+		printMutex.Lock()
+		if count == 0 {
+			printMutex.Unlock()
+			break
+		}
+
 		fmt.Println(ip)
+
 		if limited {
-			countMutex.Lock()
 			count--
-			countMutex.Unlock()
-			if count <= 0 {
+			if count == 0 {
+				printMutex.Unlock()
 				break
 			}
 		}
+		printMutex.Unlock()
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "reading standard input:", err)
-		os.Exit(255)
-	}
-	os.Exit(0)
 }
 
 func main() {
 	var wg sync.WaitGroup
-    flag.Parse()
+	flag.Parse()
+	ch := make(chan string, workers*2)
 
 	scanner := bufio.NewScanner(os.Stdin)
 	limited := count > 0
 
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
-		go Work(scanner, limited, &wg)
+		go Work(ch, limited, &wg)
+	}
+
+	for scanner.Scan() {
+		if count == 0 {
+			break
+		}
+		ch <- scanner.Text()
+	}
+
+	close(ch)
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
 
 	wg.Wait()
