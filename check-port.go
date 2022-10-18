40454e160011052a24a55793f6981012167b5ef3
Mikhail Yudin <fagci.nsk@gmail.com>
2022-10-18 23:34:22 +0700

    refactor

--- check-port.go
+++ check-port.go
@@ -11,14 +11,18 @@ import (
 )
 
 var (
-	port    string
-	workers int
-	timeout time.Duration
+	port       string
+	workers    int
+	count      int64
+	timeout    time.Duration
+	stdinMutex sync.Mutex
+	countMutex sync.Mutex
 )
 
 func init() {
 	flag.StringVar(&port, "p", "80", "port")
 	flag.IntVar(&workers, "w", 256, "workers count")
+	flag.Int64Var(&count, "c", 0, "count of IPs to gather (0 = infinite)")
 	flag.DurationVar(&timeout, "t", 750*time.Millisecond, "connection timeout")
 }
 
@@ -31,33 +35,45 @@ func CheckPort(host string, port string) bool {
 	return false
 }
 
+func Work(scanner *bufio.Scanner, limited bool, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		stdinMutex.Lock()
+		if !scanner.Scan() {
+			break
+		}
+		ip := scanner.Text()
+		stdinMutex.Unlock()
+		if !CheckPort(ip, port) {
+			continue
+		}
+		fmt.Println(ip)
+		if limited {
+			countMutex.Lock()
+			count--
+			countMutex.Unlock()
+			if count <= 0 {
+				break
+			}
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading standard input:", err)
+		os.Exit(255)
+	}
+	os.Exit(0)
+}
+
 func main() {
-	flag.Parse()
 	var wg sync.WaitGroup
-	var mutex sync.Mutex
+    flag.Parse()
+
 	scanner := bufio.NewScanner(os.Stdin)
+	limited := count > 0
 
 	for i := 0; i < workers; i++ {
 		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for {
-				mutex.Lock()
-				if !scanner.Scan() {
-					break
-				}
-				ip := scanner.Text()
-				mutex.Unlock()
-				if CheckPort(ip, port) {
-					fmt.Println(ip)
-				}
-			}
-			if err := scanner.Err(); err != nil {
-				fmt.Fprintln(os.Stderr, "reading standard input:", err)
-				os.Exit(255)
-			}
-			os.Exit(0)
-		}()
+		go Work(scanner, limited, &wg)
 	}
 
 	wg.Wait()
