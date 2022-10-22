97008553fdb6deb22eed66e4d6898f6870800351
Mikhail Yudin <fagci.nsk@gmail.com>
2022-10-22 22:23:30 +0700

    refactor

--- check-port.go
+++ check-port.go
@@ -12,11 +12,10 @@ import (
 )
 
 var (
-	port       string
-	workers    int
-	count      uint64
-	timeout    time.Duration
-	printMutex sync.Mutex
+	port    string
+	workers int
+	count   uint64
+	timeout time.Duration
 )
 
 func init() {
@@ -26,30 +25,30 @@ func init() {
 	flag.DurationVar(&timeout, "t", 750*time.Millisecond, "connection timeout")
 }
 
-func CheckPort(host string, port string, ch chan string) {
+func CheckPort(host string, port string, toGatherIPsChan chan<- string) {
 	if conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout); err == nil {
 		conn.Close()
-		ch <- host
+		toGatherIPsChan <- host
 	}
 }
 
-func Work(ch_in chan string, ch_out chan string, wg *sync.WaitGroup) {
+func Work(toCheckIPsChan <-chan string, toGatherIPsChan chan<- string, wg *sync.WaitGroup) {
 	defer wg.Done()
 
 	for {
-		if ip, ok := <-ch_in; ok {
-			CheckPort(ip, port, ch_out)
+		if ip, ok := <-toCheckIPsChan; ok {
+			CheckPort(ip, port, toGatherIPsChan)
 			continue
 		}
 		return
 	}
 }
 
-func Gather(ch chan string, wg *sync.WaitGroup) {
+func Gather(toGatherIPsChan <-chan string, wg *sync.WaitGroup) {
 	defer wg.Done()
 	limited := count > 0
 	for {
-		ip, ok := <-ch
+		ip, ok := <-toGatherIPsChan
 		if !ok {
 			break
 		}
@@ -63,49 +62,49 @@ func Gather(ch chan string, wg *sync.WaitGroup) {
 	}
 }
 
-func ReadStdin(ch_in chan string, ctx context.Context) {
+func ReadStdin(toCheckIPsChan chan<- string, ctx context.Context) {
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			ch_in <- scanner.Text()
+			toCheckIPsChan <- scanner.Text()
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading standard input:", err)
 	}
-	close(ch_in)
+	close(toCheckIPsChan)
 }
 
-func RunWorkers(ch_in, ch_out chan string) {
+func RunWorkers(toCheckIPsChan <-chan string, toGatherIPsChan chan<- string) {
 	var wg_workers sync.WaitGroup
 	for i := 0; i < workers; i++ {
 		wg_workers.Add(1)
-		go Work(ch_in, ch_out, &wg_workers)
+		go Work(toCheckIPsChan, toGatherIPsChan, &wg_workers)
 	}
 	wg_workers.Wait()
-	close(ch_out)
+	close(toGatherIPsChan)
 }
 
 func main() {
-	var wg_gather sync.WaitGroup
+	var wgGather sync.WaitGroup
 
 	flag.Parse()
 
-	ch_in := make(chan string, workers*2)
-	ch_out := make(chan string)
+	toCheckIPsChan := make(chan string, workers*2)
+	toGatherIPsChan := make(chan string)
 
-	reader_ctx, reader_cancel := context.WithCancel(context.Background())
+	readerCtx, readerCancel := context.WithCancel(context.Background())
 
-	wg_gather.Add(1)
+	wgGather.Add(1)
 
-	go Gather(ch_out, &wg_gather)
-	go RunWorkers(ch_in, ch_out)
-	go ReadStdin(ch_in, reader_ctx)
+	go Gather(toGatherIPsChan, &wgGather)
+	go RunWorkers(toCheckIPsChan, toGatherIPsChan)
+	go ReadStdin(toCheckIPsChan, readerCtx)
 
-	wg_gather.Wait()
-	reader_cancel()
+	wgGather.Wait()
+	readerCancel()
 }
